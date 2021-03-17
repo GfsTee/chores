08377aac8545f15dd3a47452dae2988e0c53d35f
Georg Treitz <georg.treitz@gmail.com>
2021-03-17 18:28:19 +0100

    second sport

--- src/Components/Training.js
+++ src/Components/Training.js
@@ -4,9 +4,9 @@ import TimerUp from './TimerUp';
 const Training = () => {
     return (
         <div className="training">
-            <span className="aside">Training</span>
-            <TimerUp headline="Sport" alert={true} days={3} />
-
+            <span className="aside">Sport</span>
+            <TimerUp headline="Org" alert={true} days={3} />
+            <TimerUp headline="Trin" alert={true} days={3} />
         </div>
     );
 }
