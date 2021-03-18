37e1d548de6a5108bb1e9c532ee14c0759333cc7
Georg Treitz <georg.treitz@gmail.com>
2021-03-18 09:03:55 +0100

    sport

--- src/Components/Training.js
+++ src/Components/Training.js
@@ -6,7 +6,7 @@ const Training = () => {
         <div className="training">
             <span className="aside">Sport</span>
             <TimerUp headline="Org" alert={true} days={3} />
-            <TimerUp headline="Trin" alert={true} days={3} />
+            <TimerUp headline="Sport" alert={true} days={3} />
         </div>
     );
 }
